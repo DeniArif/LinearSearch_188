9cf741b0db59be79b30178c767fbfb16d4e78372
DeniArif <deniarifwibowo82@gmail.com>
2023-04-14 10:38:54 +0700

    comit

--- LinearSearch_188/LinearSearch_188.cpp
+++ LinearSearch_188/LinearSearch_188.cpp
@@ -4,3 +4,17 @@ using namespace std;
 int arr[20];
 int n;
 int i;
+
+void input()
+{
+	while (true)
+	{
+		cout << "Enter the number of element int the array : ";
+		cin >> n;
+		if ((n > 0) && (n <= 20))
+			break;
+		else
+			cout << "\nArray should have minimum 1 hanf maximum 20 element.\n\n";
+	}
+
+}
